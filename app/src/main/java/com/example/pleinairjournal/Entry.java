a9347a78c3781a0c09beb2eeba3cc294ecd09997
Iris Wong <iwwong@sfu.ca>
2019-10-22 17:19:58 -0700

    added time string

--- app/src/main/java/com/example/pleinairjournal/Entry.java
+++ app/src/main/java/com/example/pleinairjournal/Entry.java
@@ -5,6 +5,7 @@ public class Entry {
     public static final String TABLE_NAME = "entry";
     public static final String UID = "_id";
     public static final String DATE = "date";
+    public static final String TIME = "time";
     public static final String LOCATION = "location";
     public static final String LONGLAT = "longlat";
     public static final String COMMENT = "comment";
